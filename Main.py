4f1964717110445033c11c8a942e92ed5227a4e4
RubenLopezB-Ast <rlb09@hotmail.es>
2025-10-14 13:05:53 +0200

    Segunda subida del proyecto 2 de EC

--- Main.py
+++ Main.py
@@ -1,4 +1,6 @@
 import os
+import shutil
+
 
 def mostrar_menu():
     print("\n===== MENÚ DE GESTIÓN DE ARCHIVOS =====")
@@ -53,6 +55,55 @@ def crear_archivo():
     except Exception as e:
         print("|||| ERROR AL CREAR EL ARCHIVO ||||")
 
+def escribir_archivo():
+    nombre = input("\n Cual es el archivo al que quieres añadir texto: ")
+    try:
+        if not os.path.isfile(nombre):
+            print("**** No encontramos ese archivo ****")
+        else:
+            texto_escrito_nuevo = input("Dime que texto quieres añadir: ")
+            with open(nombre, 'a', encoding='utf-8') as archivo:
+                archivo.write(texto_escrito_nuevo + '\n')
+            print(f"**** Has añadido texto en '{nombre}' ****")
+    except Exception as e:
+        print("|||| ERROR HAS ESCRITO DE FORMA INCORRECTA EL ACHIVO:", str(e), "||||")
+
+def eliminar():
+    nombre = input("\n Dime qué elemento quieres eliminar: ")
+    try:
+        if not os.path.exists(nombre):
+            print("**** No encontramos ese elemento ****")
+        elif os.path.isfile(nombre):
+            os.remove(nombre)
+            print(f"El archivo:  '{nombre}' ha sido eliminado.")
+        elif os.path.isdir(nombre):
+            confirmacion = input(f" La carpeta '{nombre}' contiene archivos. ¿Estas seguro de que la quieres eliminar?. Elije #SI# o #NO#")
+            if confirmacion.lower() in ["SI","Si","si","sí","Sí","SÍ"]:
+                shutil.rmtree(nombre)
+                print(f"Has eliminado la carpeta '{nombre}' y todo su contenido.")
+            else:
+                print("No has eliminado nada has cancelado la operación. ")
+        else:
+            print("**** Ese elemento no es ni una carpeta ni un archivo ****")
+    except Exception as e:
+        print("|||| ERROR NO SE HA ELIMINADO CORRECTAMENTE EL ELEMENTO: ", str(e),"||||")
+def info():
+    nombre = input("\n Te mostraré la información del archivo o carpeta: ")
+    try:
+        if not os.path.exists(nombre):
+            print("**** No encontramos ese archivo o carpeta, inténtalo de nuevo. ||||")
+        else:
+            peso = os.path.exists(nombre)
+            from datetime import datetime
+            timestamp = os.path.getmtime(nombre)
+            ultima_modificacion = datetime.fromtimestamp(timestamp).strftime('%d-%m-%Y %H:%M:%S')
+            tipo = "~CARPETA~" if os. path.isdir(nombre) else "~ARCHIVO~"
+            print(f"\n La información del archivo '{nombre}' es: ")
+            print(f"\n Fecha de la última modificación: '{ultima_modificacion}'")
+            print(f"\n El tipo de archivo es: '{tipo}'")
+            print(f"\n El tamaño del archivo es '{peso}' Bytes")
+    except Exception as e:
+        print("|||| ERROR AL MOSTRAR LA INFORMACIÓN DEL ARCHIVO: ",str(e), "||||")
 def main():
     while True:
         mostrar_menu()
@@ -65,12 +116,12 @@ def main():
         elif opcion == "3":
             crear_archivo() #Crear archivo
         elif opcion == "4":
-            pass #Escribir archivo
+            escribir_archivo() #Escribir archivo
         elif opcion == "5":
-            pass #Eliminar elemento
+            eliminar() #Eliminar elemento
         elif opcion == "6":
-            pass #Mostrar info
-        elif opcion == "7":
+            info() #Mostrar info
+        elif info() == "7":
             print("*** Regresa cuando quieras ***")
             break
         else:
